2397d1703d96379a46ded768e7923716c0f3c96e
Nickolay Muravev <murvievnick@gmail.com>
2024-05-03 10:20:53 +0300

    feat: добавил анимации

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,35 +1,54 @@
-
-const faqItemsBtns = document.querySelectorAll('.faq__item-head');
+const faqItemsBtns = document.querySelectorAll(".faq__item-head");
 
 faqItemsBtns.forEach((item) => {
-  item.addEventListener('click', () => {
+  item.addEventListener("click", () => {
     let container = item.nextElementSibling;
-    
-    if (item.classList.contains('is-open')) {
-      item.classList.remove('is-open');
+
+    if (item.classList.contains("is-open")) {
+      item.classList.remove("is-open");
       container.style.maxHeight = 0;
     } else {
-      item.classList.add('is-open');
-      container.style.maxHeight = container.scrollHeight + 'px';
+      item.classList.add("is-open");
+      container.style.maxHeight = container.scrollHeight + "px";
     }
-  })
-})
+  });
+});
 
+const modal = document.querySelector("[data-modal]");
 
-const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelectorAll("[data-modal-close]");
 
-const modalCloseBtn = document.querySelectorAll('[data-modal-close]');
-
-const modalOpenBtn = document.querySelectorAll('[data-modal-open]');
+const modalOpenBtn = document.querySelectorAll("[data-modal-open]");
 
 modalOpenBtn.forEach((item) => {
-  item.addEventListener('click', () => {
-    modal.classList.add('is-open');
-  })
-})
+  item.addEventListener("click", () => {
+    modal.classList.add("is-open");
+  });
+});
 
 modalCloseBtn.forEach((item) => {
-  item.addEventListener('click', () => {
-    modal.classList.remove('is-open');
-  })
-})
+  item.addEventListener("click", () => {
+    modal.classList.remove("is-open");
+  });
+});
+
+let counters = document.querySelectorAll(".number");
+
+counters.forEach((item) => {
+  let numberTop = item.getBoundingClientRect().top;
+  let start = +item.dataset.min;
+  let end = +item.dataset.max;
+
+  window.addEventListener("scroll", function onScroll() {
+    if (window.pageYOffset > numberTop - window.innerHeight / 2) {
+      this.removeEventListener("scroll", onScroll);
+      var interval = setInterval(function () {
+        item.innerHTML = ++start;
+        item.classList.add('is-visible')
+        if (start == end) {
+          clearInterval(interval);
+        }
+      }, 5);
+    }
+  });
+});
